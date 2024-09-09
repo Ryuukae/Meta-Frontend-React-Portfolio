66a34514db379ad68aa97c6f362664a4990930a4
Ryuukae <adamdes.dev@gmail.com>
2024-09-09 03:43:31 -0400

    Update and implement ContactMeSection, Card, and LandingSection
    Refactored ContactMeSection to handle form submissions with validation using Formik and Yup. Implemented the Card component UI with Chakra UI for displaying images, titles, and descriptions. Completed the LandingSection component by adding an avatar and detailed headings.
    
    Signed-off-by: Ryuukae <ryuukae@gmail.com>

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,23 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {Box, Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+const Card = ({title, description, imageSrc}) => {
+    return (
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" backgroundColor="white">
+            <Image width="100%" src={imageSrc} alt={title}/>
+
+            <VStack p="6" align="start" spacing={3}>
+                <Heading size="md" color="black">{title}</Heading>
+                <Text color="black">{description}</Text>
+            </VStack>
+            <HStack p="6" justify="flex-start">
+                <Text color="black">See more</Text>
+                <FontAwesomeIcon color="black" icon={faArrowRight} size="1x"/>
+            </HStack>
+        </Box>
+    );
 };
 
 export default Card;
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import {Avatar, Heading, VStack} from "@chakra-ui/react";
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
+const avatarUrl = "https://i.pravatar.cc/150?img=7";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-
-  </FullScreenSection>
+    <FullScreenSection
+        justifyContent="center"
+        alignItems="center"
+        isDarkBackground
+        backgroundColor="#2A4365"
+    >
+        <Avatar src={avatarUrl} alt="Pete's Avatar"/>
+        <VStack spacing={4}>
+            <Heading size="2xl">{greeting}</Heading>
+            <Heading size="lg">{bio1}</Heading>
+            <Heading size="md">{bio2}</Heading>
+        </VStack>
+    </FullScreenSection>
 );
 
 export default LandingSection;
